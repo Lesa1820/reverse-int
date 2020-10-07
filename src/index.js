4460e44ff42c38608c184e617d0c11b162c2a04a
lesa1820 <lesa182011@gmail.com>
2020-10-07 13:50:30 +0300

    docs:update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n)
+  if(n > 9){
+      n = n.toString().split("").reverse().join("");
+      return n;
+  }
+  else{
+    return n;
+  }
 }
